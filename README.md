598cc4bb0a3115d1b223b3072646709cad7e0a6b
azu0609 <ipadgoogcloud@gmail.com>
2023-02-09 17:16:05 +0900

    fix script(maybe?)

--- README.md
+++ README.md
@@ -8,9 +8,9 @@
 ## Included IPA:
 | Type |Application | Bundle | Original ver | Version | Author |
 |:----:|:----------:|:------:|:------------:|:-------:|:------:|
-| Tweaked app | [uYou](https://github.com/MiRO92/uYou-for-YouTube) | com.google.ios.youtube | 16.42.3 | 2.1 | MIRO92 |
-| Tweaked app | [uYou+](https://github.com/qnblackcat/uYouPlus) | com.google.ios.youtube | 18.01.6 | 2.3.1 | qnblackcat |
-| Tweaked app | [Enmity](https://enmity.app) | com.hammerandchisel.discord | 158.0 | 2.1.4 | enmity-mod |
-| Tweaked app | [Enmity (dev)](https://enmity.app) | com.hammerandchisel.discord | 158.0 | 2.1.4 | enmity-mod |
-| App | [Anime Now!](https://github.com/AnimeNow-Team/AnimeNow) | com.errorerrorerror.animenow (maybe) | None | 1.0.0 | errorerrorerror |
-| Macdirtycow | [Cowabunga](https://github.com/leminlimez/Cowabunga) | com.leemin.passthm (maybe) | None | 7.0.4 | leminlimez
+| Tweaked app | [uYou](https://github.com/MiRO92/uYou-for-YouTube) | com.google.ios.youtube | 16.42.3 | 2.1 | [MIRO92](https://github.com/MiRO92) |
+| Tweaked app | [uYou+](https://github.com/qnblackcat/uYouPlus) | com.google.ios.youtube | 18.01.6 | 2.3.1 | [qnblackcat](https://github.com/qnblackcat) |
+| Tweaked app | [Enmity](https://enmity.app) | com.hammerandchisel.discord | 158.0 | 2.1.4 | [enmity-mod](https://github.com/enmity-mod) |
+| Tweaked app | [Enmity (dev)](https://enmity.app) | com.hammerandchisel.discord | 158.0 | 2.1.4 | [enmity-mod](https://github.com/enmity-mod) |
+| App | [Anime Now!](https://github.com/AnimeNow-Team/AnimeNow) | com.errorerrorerror.animenow | None | 1.0.0 | [errorerrorerror](https://github.com/errorerrorerror) |
+| Macdirtycow | [Cowabunga](https://github.com/leminlimez/Cowabunga) | com.leemin.Cowabunga | None | 7.0.4 | [leminlimez](https://github.com/leminlimez)
--- script/fetch.py
+++ script/fetch.py
@@ -6,18 +6,18 @@ class Fetch:
         self.src = ["https://api.github.com/repos/enmity-mod/tweak/releases/latest"]
         self.current_ver = ["2.1.3"]
 
-    def fetch(self, app):
-        version = requests.get(self.src[app]).json()["name"]
+    def fetch(self, index):
+        version = requests.get(self.src[index]).json()["name"]
         version = version.strip("v")
-        if version > self.current_ver[app]:
+        if version > self.current_ver[index]:
             print(f"{self.current_ver} >>> {version}")
             print("New version found! - Replacing...")
-            self.read("../scarlet_repo.json", version, app)
+            self.read("/home/azuki/Documents/repo/scarlet_repo.json", version, index)
         else:
             print("Latest.")
 
     
-    def read(self, path, version, app):
+    def read(self, path, version, index):
         """
         print(version)
         with open(path, "r") as target:
@@ -27,15 +27,18 @@ class Fetch:
         with open(path, "w") as w:
             json.dump(data, w, indent=4)
         """
+        # with open(path, "r") as file:
+            # json_data = json.load(file)
+            # print("a" + json_data["Tweaked"][0]["version"])
+        # print()
         
-        with open(path, "r") as target:
-            data = json.load(target)
-            for item in data:
-                if item["version"] in [self.current_ver[app]]:
-                    item["version"] = version
+        with open(path, 'r') as file:
+            json_data = json.load(file)
+            if json_data["Tweaked"][index]["version"] == self.current_ver:
+                json_data["Tweaked"][index]["version"] = version
+        with open(path, 'w') as file:
+            json.dump(json_data, file, indent=2)
         
-        with open(path, "w") as w:
-            json.dump(data, w, indent=2)
         
 
 if __name__ == "__main__":
