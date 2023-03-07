48515c31e30611a9cebf3ad277eb9139109802fc
azu0609 <ipadgoogcloud@gmail.com>
2023-03-07 22:05:40 +0900

    Feat: use 1 list instead of 3 to support altstore

--- script/fetch.py
+++ script/fetch.py
@@ -4,7 +4,12 @@ import sys
 
 class Fetch:
     def __init__(self):
-        
+        self.release_source = ["https://api.github.com/repos/acquitelol/rosiecord/releases",
+                               "https://api.github.com/repos/enmity-mod/tweak/releases",
+                               "https://api.github.com/repos/qnblackcat/uYouPlus/releases",
+                               "https://api.github.com/repos/AnimeNow-Team/AnimeNow/releases",
+                               "https://api.github.com/repos/leminlimez/Cowabunga/releases",
+                               "https://api.github.com/repos/haxi0/KillMyOTA/releases"]
         self.Tweaked = ["https://api.github.com/repos/enmity-mod/tweak/releases",
                         "https://api.github.com/repos/enmity-mod/tweak/releases",
                         "https://api.github.com/repos/qnblackcat/uYouPlus/releases"]
@@ -13,27 +18,37 @@ class Fetch:
                             "https://api.github.com/repos/haxi0/KillMyOTA/releases"]
 
 
-    def logger(self, type, message):
-        if type == "info":
+    def logger(self, type: int, message):
+        if type == 0:
             print("INFO: " + message)
-        elif type == "debug":
+        elif type == 1:
             print("DEBUG: " + message)
-        elif type == "warn":
+        elif type == 2:
             print("WARN: " + message)
-        elif type == "error":
+        elif type == 3:
             print("ERROR: " + message)
         else:
             print(message)
     
 
-    def fetch(self, index, app_type, current_ver):
+    def fetch(self, repo, index, app_type, current_ver, download_url):
         if index == 3:
             version = "v2.1"
-            self.logger("info", "uYou detected! using 2.1 instend of latest.")
+            self.logger(2, "uYou detected! using 2.1 instend of latest.")
         else:
             version = None
             changelog = None
             released_date = None
+            for i, releases in enumerate(self.release_source):
+                if releases.replace("api.", "").replace("repos/", "") in download_url:
+                    self.logger(0, "Found url.")
+                    try:
+                        version = requests.get(self.release_source[i]).json()[0]["name"]
+                        changelog = requests.get(self.release_source[i]).json()[0]["body"]
+                        released_date = requests.get(self.release_source[i]).json()[0]["assets"][0]["created_at"]
+                    except KeyError:
+                        raise("Rate limited")
+            """
             if app_type == "Macdirtycow":
                 version = requests.get(self.Macdirtycow[index]).json()[0]["name"]
                 changelog = requests.get(self.Macdirtycow[index]).json()[0]["body"]
@@ -46,39 +61,39 @@ class Fetch:
                 version = requests.get(self.Tweaked[index]).json()[0]["name"]
                 changelog = requests.get(self.Tweaked[index]).json()[0]["body"]
                 released_date = requests.get(self.Tweaked[index]).json()[0]["assets"][0]["created_at"]
-        try:
-            version = version.strip("v")
-        except AttributeError:
-            self.logger("error", "Rate limited by Github. can't use github api for a while")
-            raise()
+            """
+        version = version.strip("v")
         # released_date = released_date.find("T")[:released_date] # Disabled due to error. available in soon
-        self.logger("debug", f"index: {index}, current: {current_ver}, new: {version}") # Disabled due to error
+        self.logger(1, f"index: {index}, current: {current_ver}, new: {version}") # Disabled due to error
         if version > current_ver:
-            self.logger("info", f"New version available: {version}, updating...")
-            self.rw("altstore", "../altstore_repo.json", "../scarlet_repo.json", version, index, app_type, current_ver, changelog, released_date)
+            self.logger(0, f"New version available: {version}, updating...")
+            self.rw(repo, "../altstore_repo.json", "../scarlet_repo.json", version, index, app_type, current_ver, changelog, released_date)
         else:
-            self.logger("info", "Up to date.")
+            self.logger(0, "Up to date.")
 
     
-    def rw(self, type, altstore_path, scarlet_path, version, index, app_type, current_ver, version_description, release_date):
-            if type == "scarlet":
+    def rw(self, repo_type, altstore_path, scarlet_path, version, index, app_type, current_ver, version_description, release_date):
+            if repo_type == "scarlet":
                 with open(scarlet_path, "r") as scarlet_repo:
-                    self.logger("info", "Loading data...")
+                    self.logger(0, "Loading data...")
                     json_data = json.load(scarlet_repo)
-                    self.logger("info", "Modifying loaded data...")
+                    self.logger(0, "Modifying loaded data...")
                     json_data[app_type][index]["version"] = version
                     json_data[app_type][index]["down"] = json_data[app_type][index]["down"].replace(current_ver, version)
                     if index == 2:
                         json_data[app_type][index]["down"] = json_data[app_type][index]["down"].replace(current_ver.replace("-", "_"), version.replace("-", "_"))
                 with open(scarlet_path, 'w') as scarlet_repo:
                     json.dump(json_data, scarlet_repo, indent=2)
-                    self.logger("info", f"Writed to: {scarlet_path} successfully.")
+                    self.logger(0, f"Writed to: {scarlet_path} successfully.")
             
-            elif type == "altstore":
+            elif repo_type == "altstore":
                 with open(altstore_path, "r") as altstore_repo:
-                    self.logger("info", "Loading json manifest... this may take a while")
+                    self.logger(0, "Loading json manifest... this may take a while")
                     json_data = json.load(altstore_repo)
-                    self.logger("info", "Modifying loaded data...")
+                    self.logger(0, "Modifying loaded data...")
+                    print(type(index))
+                    print(type(app_type))
+                    print(type(version))
                     json_data[app_type][index]["version"] = version
                     json_data[app_type][index]["down"] = json_data[app_type][index]["down"].replace(current_ver, version)
                     json_data[app_type][index]["versionDescription"] = version_description
@@ -87,14 +102,14 @@ class Fetch:
                         json_data[app_type][index]["down"] = json_data[app_type][index]["down"].replace(current_ver.replace("-", "_"), version.replace("-", "_"))
                 with open(altstore_path, "w") as altstore_repo:
                     json.dump(json_data, altstore_repo, indent=2)
-                    self.logger("info", f"Writed to: {altstore_path} successfully.")
+                    self.logger(0, f"Writed to: {altstore_path} successfully.")
             
             else:
                 raise("Unexpected mode")
             """
             # Modify readme
             with open("../README.md", "r") as file:
-                self.logger("info", "Loading readme.md data...")
+                self.logger(0, "Loading readme.md data...")
                 file_data = file.readlines()
                 for line in file_data:
                     print(line)
@@ -102,7 +117,7 @@ class Fetch:
                         print(line)
                 data = file.read()
             with open("../README.md", "w") as file:
-                self.logger("info", "Writing modified data...")
+                self.logger(0, "Writing modified data...")
                 file.write(data.replace(current_ver, version))
             """
     
@@ -113,22 +128,27 @@ class Fetch:
             for item in json_data:
                 for i, key in enumerate(json_data[item]):
                     try:
-                        self.fetch(i, item, key["version"])
+                        if path == "../altstore_repo.json":
+                            self.fetch("altstore", i, item, key["version"], key["downloadURL"])
+                        elif path == "../scarlet_repo.json":
+                            self.fetch("scarlet", i, item, key["version"], key["down"])
+                        else:
+                            raise("Unexpected repo!")
                     except TypeError as e:
                         if str(e) == "string indices must be integers":
-                            self.logger("warn", f"{e}, this seems caused by metadata.")
+                            self.logger(2, f"{e}, this seems caused by metadata.")
                         else:
                             raise(e)
-        self.logger("info", f"All done! may take 1~2m(Page build time) to apply.")
+        self.logger(0, f"All done! may take 1~2m(Page build time) to apply.")
 
 
 if __name__ == "__main__":
     try:
         if sys.argv[1] == "--production":
-            Fetch().automate("../altstore_repo.json")
+            Fetch().automate("../scarlet_repo.json")
         if sys.argv[1] == "--test":
             # try:
-            Fetch().fetch(sys.argv[3], "apps", sys.argv[2])
+            Fetch().fetch("altstore", sys.argv[3], "apps", sys.argv[2], "https://github.com/enmity-mod/tweak/releases")
             # except IndexError:
                 # print("ERROR: Needed argument not found. example: 2.1.4 Enmity")
     except IndexError:
